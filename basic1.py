c1da6b20fa7ba4667eb4376f1b5174b3010b6af8
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-01 20:15:14 +0700

    1.0.2(resize)

--- basic1.py
+++ basic1.py
@@ -1,6 +1,8 @@
 import cv2
 img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
+imgresize = cv2.resize(img,(400,400))
 
-cv2.imshow("Outpen",img)#แสดงผลภาพ
-cv2.waitKey(delay=0)
+cv2.imshow("Outpen",imgresize)#แสดงผลภาพ
+cv2.waitKey(0)#delay
+
 cv2.destroyAllWindows()#คืนทรัพร์ยากรให้windows
