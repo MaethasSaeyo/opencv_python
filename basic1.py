9c5af849d34d2fbce65b4c78a546b315d68d1ff2
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-01 20:11:05 +0700

    1.0.1(แสดงผลภาพ)

--- basic1.py
+++ basic1.py
@@ -1,5 +1,6 @@
 import cv2
 img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
 
-print(type(img.ndim))
-print(img)
+cv2.imshow("Outpen",img)#แสดงผลภาพ
+cv2.waitKey(delay=0)
+cv2.destroyAllWindows()#คืนทรัพร์ยากรให้windows
