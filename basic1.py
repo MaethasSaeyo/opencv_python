9369216461829e2aacc47e6ebf9519b976e60cbe
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-02 18:06:27 +0700

    1.0.9(วาดสี่เหลี่ยม)

--- basic1.py
+++ basic1.py
@@ -2,9 +2,10 @@ import cv2
 img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
 imgresize = cv2.resize(img,(400,400))
 
-#วาดเส้นตรง
-#line(ภาพ,จุดเริ่มต้น(x,y),จุดสุดท้าย(x,y),สี(BGR),ความหนา)
-cv2.arrowedLine(imgresize,(0,200),(150,200),(0,0,255),5)
+#วาดสี่เหลี่ยม
+#rectangle(ภาพ,มุมที่1(บนซ้าย),มุมที่2(ล่างขวา),สี(BGR),ความหนา)
+cv2.rectangle(imgresize,(0,0),(100,200),(0,0,255),1)
+
 
 cv2.imshow("Outpen",imgresize)#แสดงผลภาพ
 cv2.waitKey(0)#delay
