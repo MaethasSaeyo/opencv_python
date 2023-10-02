94993e8bd43c0afb81fdc501002f2e047c4f8b11
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-02 16:22:07 +0700

    1.0.5(อ่านวีดีโอ)

--- basic1.py
+++ basic1.py
@@ -1,14 +1,17 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("D:/2023/opencv/cv_1/opencv_python/image/Video (1).mp4")
 
-while (True):
-    chack , frame = cap.read()#รับภาพจากกล้องframe : frame
-    cv2.imshow("Output",frame)
+while (cap.isOpened()):
+    check , frame = cap.read() #รับภาพจากกล้อง frame ต่อ frame
+    
+    if check == True:#checkว่าวีดีโอยังเล่นอยู่รึเปล่า
+        cv2.imshow("Output",frame)
 
-    if cv2.waitKey(1) & 0xff == ord("e"):#กดeเพื่อปิดกล้อง
+        if cv2.waitKey(1) & 0xff == ord("e"):#กดeเพื่อปิดvideo
+            break
+    else:
         break
-
 
 cap.release()#เครียแรม
 cv2.destroyAllWindows()
