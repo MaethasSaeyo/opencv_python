d435c14e053e82ca6816fe3826b8e59329f22c13
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-02 18:21:19 +0700

    1.1.1(ข้อความ)

--- basic1.py
+++ basic1.py
@@ -2,9 +2,9 @@ import cv2
 img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
 imgresize = cv2.resize(img,(400,400))
 
-#วาดวงกลม
-#circle(ภาพ,ตำแหน่งจุดศูนย์กลาง(x,y),รัศมี,สี(BGR),ความหนา)
-cv2.circle(imgresize,(250,190),100,(0,0,255),5)
+#วาดข้อความ
+#putText(ภาพ,ข้อความ,พิกัดที่จะใส่ข้อความ(x,y),font,ขนาดข้อความ,สี,ความหนา)
+cv2.putText(imgresize,"Hello World",(100,200),3,1,(0,0,255),2)
 
 
 cv2.imshow("Outpen",imgresize)#แสดงผลภาพ
