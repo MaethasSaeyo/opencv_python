c8736dee41e2a1b7f9556d2d0f4a47093024a7ab
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-02 15:42:37 +0700

    1.0.3(เขียนภาพ)

--- basic1.py
+++ basic1.py
@@ -1,8 +1,10 @@
 import cv2
-img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
+img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg",0)
 imgresize = cv2.resize(img,(400,400))
 
-cv2.imshow("Outpen",imgresize)#แสดงผลภาพ
-cv2.waitKey(0)#delay
+cv2.imshow("My Cat",imgresize)
 
-cv2.destroyAllWindows()#คืนทรัพร์ยากรให้windows
+cv2.imwrite("output.jpg",imgresize)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
