cd5d25e94b062b1515aeb05136247957c3278854
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-02 18:12:00 +0700

    1.1.0(วาดวงกลม)

--- basic1.py
+++ basic1.py
@@ -2,9 +2,9 @@ import cv2
 img = cv2.imread("D:/2023/opencv/cv_1/opencv_python/image/cat.jpg")
 imgresize = cv2.resize(img,(400,400))
 
-#วาดสี่เหลี่ยม
-#rectangle(ภาพ,มุมที่1(บนซ้าย),มุมที่2(ล่างขวา),สี(BGR),ความหนา)
-cv2.rectangle(imgresize,(0,0),(100,200),(0,0,255),1)
+#วาดวงกลม
+#circle(ภาพ,ตำแหน่งจุดศูนย์กลาง(x,y),รัศมี,สี(BGR),ความหนา)
+cv2.circle(imgresize,(250,190),100,(0,0,255),5)
 
 
 cv2.imshow("Outpen",imgresize)#แสดงผลภาพ
