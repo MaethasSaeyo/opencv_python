8f9e592384da2a747b60727a9663f3b87adfa7d8
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-05 15:18:34 +0700

    1.1.6(สร้างเส้นเชื่อมโยง)

--- basic1.py
+++ basic1.py
@@ -1,20 +1,24 @@
 #Mouse Event
 import cv2
 import numpy as np
-img = cv2.imread("image/cat.jpg")
-imgresize = cv2.resize(img,(700,700))
+img =np.zeros([400,500,3])
+
+points = []
+
 def click(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        blue = imgresize[y,x,0]
-        green = imgresize[y,x,1]
-        red = imgresize[y,x,2]
-        imgcolor = np.zeros([300,300,3],np.uint8)
-        imgcolor[:] = [blue,green,red]
-        cv2.imshow("Result",imgcolor)
+        cv2.circle(img,(x,y),10,(0,0,255),4)
+        points.append((x,y))
+        if len(points) >= 2:
+            cv2.line(img,points[-2],points[-1],(255,0,80),5)
+
+
+        cv2.imshow("Output",img)
+        
 
 
 
-cv2.imshow("Output",imgresize)#แสดงผลภาพ
+cv2.imshow("Output",img)#แสดงผลภาพ
 
 #ทำงานกับmouse
 cv2.setMouseCallback("Output",click)
