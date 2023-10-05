bdb80f5ad68a36f06b536d1f763455086b16a435
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-05 13:57:04 +0700

    1.1.4(ตรวจจับค่าสี)

--- basic1.py
+++ basic1.py
@@ -1,16 +1,19 @@
 #Mouse Event
 import cv2
-img = cv2.imread("output.jpg")
-
+img = cv2.imread("image/cat.jpg")
+imgresize = cv2.resize(img,(700,700))
 def click(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        text = str(x)+","+str(y)
-        cv2.putText(img,text,(x,y),1,1,(0,0,255,),2)
-        cv2.imshow("Output",img)
+        blue = imgresize[y,x,0]
+        green = imgresize[y,x,1]
+        red = imgresize[y,x,2]
+        text = "BGR = "+str(blue)+","+str(green)+","+str(red)
+        cv2.putText(imgresize,text,(x,y),1,1,(0,0,255,),2)
+        cv2.imshow("Output",imgresize)
 
 
 
-cv2.imshow("Output",img)#แสดงผลภาพ
+cv2.imshow("Output",imgresize)#แสดงผลภาพ
 
 #ทำงานกับmouse
 cv2.setMouseCallback("Output",click)
