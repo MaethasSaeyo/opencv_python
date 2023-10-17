451f9646d16835377598669f97c4d52a3536d567
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-10-17 21:34:23 +0700

    1.1.9(ตรวจจับใบหน้าจากวีดีโอ)

--- basic1.py
+++ basic1.py
@@ -1,22 +1,29 @@
 import cv2
 
-img = cv2.imread("image/boy.jpg")
 
 #อ่านไฟล์สำหรับ classifficaation
 face_cascade = cv2.CascadeClassifier("Detect/haarcascades_haarcascade_frontalface_default.xml")
 
-gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-#จำแนกใบหน้า
-scaleFactor = 1.1
-minNeighber = 3
-face_detect = face_cascade.detectMultiScale(gray_img,scaleFactor,minNeighber)
-
-#แสดงตำแหน่งที่เจอในหน้า
-for (x,y,w,h) in face_detect:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+cap = cv2.VideoCapture("image/Video (1).mp4")
 
 
-cv2.imshow("Output",img)
-cv2.waitKey(0)
+
+while (cap.isOpened()):
+    check, frame = cap.read()
+    if check == True :
+        gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        #จำแนกใบหน้า
+        face_detect = face_cascade.detectMultiScale(gray_img,1.1,4)
+
+        for (x,y,w,h) in face_detect:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),5)
+
+        cv2.imshow("Output",frame)
+
+        if cv2.waitKey(1) & 0xff == ord("e"):
+            break
+    else:
+        break
+
+cap.release()
 cv2.destroyAllWindows()
